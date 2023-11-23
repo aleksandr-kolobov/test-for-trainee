42e88beb9079090acea24a22c14214c58ef74885
aleksandr-kolobov <forward2010@inbox.ru>
2023-11-23 14:17:46 +0700

    Исправил беду с лишними строками

--- WorldSights/src/main/java/main/controller/TownController.java
+++ WorldSights/src/main/java/main/controller/TownController.java
@@ -20,134 +20,59 @@ public class TownController {
 
     @GetMapping("/towns")
     public List<Town> getTownsList(@RequestParam(defaultValue = "false") boolean sort){
-
         ArrayList<Town> arrayList = new ArrayList<>();
-
         arrayList.addAll(townRepository.findAll());
-
         arrayList.sort(Comparator.comparing(Town::getId));
-
         if (sort) {
-
             arrayList.sort(Comparator.comparing(Town::getName));
-
         }
-
         return arrayList;
-
     }
-
-
 
     @GetMapping("/towns/{nameOrId}")
-
     public ResponseEntity<Town> getTown(@PathVariable String nameOrId) {
-
         nameOrId = nameOrId.substring(0, 1).toUpperCase() + nameOrId.substring(1).toLowerCase();
-
         Optional<Town> optional = townRepository.findByName(nameOrId);
-
         if (optional.isPresent()) {
-
             return new ResponseEntity(optional.get(), HttpStatus.OK);
-
         }
-
         optional = townRepository.findById(Integer.parseInt(nameOrId));
-
         return !optional.isPresent() ? ResponseEntity.notFound().build() :
-
                 new ResponseEntity(optional.get(), HttpStatus.OK);
-
     }
-
-
 
     @PostMapping("/towns")
-
     public ResponseEntity<Town> addTown(Town town) {
-
         String str = town.getName();
-
         town.setName(str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase());
-
         str = town.getCountry();
-
         town.setCountry(str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase());
-
         return new ResponseEntity(townRepository.save(town), HttpStatus.OK);
-
     }
-
-//еще такой способ возвращать JSON ответ {"result":true}
-
-
-/*    @PostMapping("/towns/")
-
-    public HashMap<String, Boolean> addTown(Town1 town) {
-
-        townRepository.save(town);
-
-        return Map.of("result", true);
-
-    }
-//или такой способ возвращать ID {"id":5}
-
-    @PostMapping("/towns/")
-
-    public HashMap<String, Integer> addTown(Town1 town) {
-
-        return Map.of("result", townRepository.save(town).getId());
-
-    }*/
-
-
 
     @PatchMapping("/towns")
-
     public ResponseEntity<Town> correctTownByName(String name, boolean subway, int population) {
         name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
-
         Optional<Town> optional = townRepository.findByName(name);
-
         if(optional.isPresent()) {
-
             Town town = optional.get();
-
             town.setPopulation(population == 0 ? town.getPopulation() : population);
-
             town.setSubway(subway);
-
             return new ResponseEntity(townRepository.save(town), HttpStatus.OK);
-
         }
-
         return ResponseEntity.notFound().build();
-
     }
-
-
 
     @PatchMapping("/towns/{id}")
-
     public ResponseEntity<Town> correctTownById(@PathVariable int id, int population, boolean subway) {
-
         Optional<Town> optional = townRepository.findById(id);
-
         if(optional.isPresent()) {
-
             Town town = optional.get();
-
             town.setPopulation(population == 0 ? town.getPopulation() : population);
-
             town.setSubway(subway);
-
             return new ResponseEntity(townRepository.save(town), HttpStatus.OK);
-
         }
         return ResponseEntity.notFound().build();
-
     }
-
 
 }
